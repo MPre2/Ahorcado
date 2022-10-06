3aae41bfadc1c6cac77ebe54676ef90964174a1f
Matias <111287993+MPre2@users.noreply.github.com>
2022-10-05 21:10:00 -0300

    Add files...

---
+++ ahorcado.js
@@ -0,0 +1,198 @@
+function dibujarCanvas() {
+    tablero.lineWidth = 8;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    tablero.strokeStyle = "#0A3871";
+    //Dibuja la base de la horca
+    tablero.fillRect(0,0,1200,860);
+    tablero.beginPath();
+    tablero.moveTo(600, 500);
+    tablero.lineTo(900, 500);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarLinea() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    tablero.strokeStyle = "#0A3871";
+    //Dibuja las lineas correspondientes a cada letra de la palabra
+    let anchura = 600/palabra.length;
+    for (let i = 0; i < palabra.length; i++){
+        tablero.moveTo(500 + (anchura*i), 640)
+        tablero.lineTo(550 + (anchura*i), 640)
+    }
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarMastil() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    tablero.strokeStyle = "#0A3871";
+    //Dibujamos el mastil del Ahorcado
+    tablero.beginPath();
+    tablero.moveTo(700, 500);
+    tablero.lineTo(700, 250);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarHorca() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    tablero.strokeStyle = "#0A3871";
+    //Dibujamos la Horca
+    tablero.beginPath();
+    tablero.moveTo(700, 250);
+    tablero.lineTo(810, 250);
+    tablero.moveTo(810, 250);
+    tablero.lineTo(810, 275);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarCabeza() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "darkblue";
+    tablero.strokeStyle = "#0A3871";
+    //Dibujamos la cabeza del Ahorcado
+    tablero.beginPath();
+    tablero.arc(810, 300, 25, 0, 2*3.14);
+    tablero.fill();
+    tablero.fillStyle = "rgb(243, 245, 252)";
+    tablero.beginPath();
+    tablero.arc(810, 300, 19, 0, 2*3.14);
+    tablero.fill();
+}
+
+function dibujarCuerpo() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    tablero.strokeStyle = "#0A3871";
+    //Dibujamos el cuerpo del Ahorcado
+    tablero.beginPath();
+    tablero.moveTo(810, 325);
+    tablero.lineTo(810, 425);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarBrazoDerecho() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    tablero.strokeStyle = "#0A3871";
+    //Dibujamos el brazo derecho del Ahorcado
+    tablero.beginPath();
+    tablero.moveTo(810, 335);
+    tablero.lineTo(780, 385);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarBrazoIzquierdo() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    tablero.strokeStyle = "#0A3871";
+    //Dibujamos el brazo izquierdo del ahorcado
+    tablero.beginPath();
+    tablero.moveTo(810, 335);
+    tablero.lineTo(840, 385);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarPiernaDerecha() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    tablero.strokeStyle = "#0A3871";
+    //Dibujamos la pierna derecha del Ahorcado
+    tablero.beginPath();
+    tablero.moveTo(810, 425);
+    tablero.lineTo(780, 475);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarPiernaIzquierda() {
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    tablero.strokeStyle = "#0A3871";
+    //Dibujamos la pierna izquierda del ahorcado
+    tablero.beginPath();
+    tablero.moveTo(810, 425);
+    tablero.lineTo(840, 475);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function escribirLetraCorrecta(index) {
+    tablero.font = "bold 52px inter";
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "darkblue";
+    //Escribe las letras correctas de la palabra en su respectivo espacio en la pantalla
+    let anchura = 600/palabra.length;
+    tablero.fillText(palabra[index], 505 + (anchura*index), 620);
+    //tablero.stroke();
+}
+
+function escribirLetraIncorrecta(letra, errorLeft) {
+    tablero.font = "bold 40px inter";
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "darkblue";
+    //Escribe las letras erradas en la pantalla
+    let anchura = 600/palabra.length;
+    tablero.fillText(letra, 505 + (40*(10-errorLeft)), 720, 40);
+}
+
+//Dibuja cada una de las partes del ahorcado
+function dibujo(errados) {
+    if (errados === 8) {
+        dibujarMastil();
+    }
+    if (errados === 7) {
+        dibujarHorca();
+    }
+    if (errados === 6) {
+        dibujarCabeza();
+    }
+    if (errados === 5) {
+        dibujarCuerpo();
+    }
+    if (errados === 4) {
+        dibujarBrazoDerecho();
+    }
+    if (errados === 3) {
+        dibujarBrazoIzquierdo();
+    }
+    if (errados === 2) {
+        dibujarPiernaDerecha();
+    }
+    if (errados === 1) {
+        dibujarPiernaIzquierda();
+    }
+}
+
---
+++ juego.js
@@ -0,0 +1,107 @@
+//Objetos del HTML
+document.querySelector("canvas");
+document.getElementById("botonInicio");
+document.getElementById("agregar-palabra").style.display = "none";
+document.getElementById("nuevoJuego").style.display = "none";
+document.getElementById("desistir").style.display = "none";
+document.getElementById("volver").style.display = "none";
+
+
+//Variables o selectores
+let palabraSecreta = ['HELLO', 'WELCOME', 'ORACLE', 'ALURA', 'JAVASCRIPT', 'CSS', 'HTML', 'BOOTSTRAP', 'VUE', 'REACT'];
+let palabra = "";
+let tablero = document.getElementById("horca").getContext("2d");
+let letras = [];
+let errores = 8;
+
+//Funcion para que el Id "agregar-palabra" aparezca en pantalla
+function apareceAgregarPalabra() {
+  document.getElementById("inicio-juego").style.display = "none";
+  document.getElementById("agregar-palabra").style.display = "inline";
+}
+
+//Función para qeu el Id "inicio-juego" aparezca en pantalla
+function apareceInicio() {
+  document.getElementById("inicio-juego").style.display = "inline";
+  document.getElementById("agregar-palabra").style.display ="none";
+}
+
+//Función para generar de manera aleatoria la palabra a descifrar
+function palabraAleatoria() {
+    palabra = palabraSecreta[Math.floor(Math.random() * palabraSecreta.length)];
+}
+
+//Funcion que comprueba el ingreso de la letra
+function comprobarLetra(key) {
+    let estado = false;
+    if (key >= 65 && letras.indexOf(key) || key <= 90 && letras.indexOf(key)) {
+      letras.push(key);
+      return estado;
+    } 
+    else {
+      estado = true;
+      return estado;
+    }
+}
+
+//Funcion que disminuye en 1 los errores que se pueden cometer
+function anadirLetraIncorrecta() {
+  errores -= 1;
+}
+
+//Funcion se realiza toda la accion del juego en si
+function juego() {
+    document.getElementById("inicio-juego").style.display = "none";
+    document.getElementById("agregar-palabra").style.display = "none";
+    document.getElementById("nuevoJuego").style.display = "inline";
+    document.getElementById("desistir").style.display = "inline";
+    document.getElementById("volver").style.display ="inline";
+    //Escoje una palabra al azar
+    palabraAleatoria();
+    //Dibuja el tablero donde se va a dibujar el ahorcado
+    dibujarCanvas();
+    //Dibuja las lineas correspondietes al total de las letras de la palabra
+    dibujarLinea();
+    //Definimos variable para identificar la cantidad de aciertos
+    let aciertos = 0;
+    //Funcion que capta la letra ingresada por teclado
+    document.onkeydown = (e) => {
+      let letra = e.key.toUpperCase();
+      if (comprobarLetra(letra) && palabra.includes(letra)) {
+        for (let i = 0; i < palabra.length; i++) {
+          if (palabra[i] === letra) {
+            escribirLetraCorrecta(i);
+            aciertos++;
+            if (aciertos == palabra.length) {
+              //Funcion para dibujar el mensaje de ganador en pantalla
+              alert('Felicitaciones, has ganado');
+              errores = 8;
+            }
+          }
+        } 
+      } 
+      else {
+        dibujo(errores);
+        escribirLetraIncorrecta(letra, errores);
+        anadirLetraIncorrecta();
+        if (errores == 0) {
+          //Funcion para dibujar por pantalla el mensaje de perdedor
+          alert('Has perdido, la palabra era ' + palabra);
+          errores = 8;
+        }
+      }
+    }
+  }
+
+//Función para agregar una palabra a la lista predefinida
+function agregarPalabra() {
+    valor = "";
+    valor = document.getElementById("ingresoPalabra").value;
+    palabraSecreta.push(valor.toUpperCase());
+    alert('La palabra a sido agregada exitosamente');
+}
+
+//Funcion que emite mensaje cuando el usuario desiste de continuar jugando
+function desistir() {
+    alert('Usted se ha dado por vencido, la palabra era: '+ palabra);
+}
